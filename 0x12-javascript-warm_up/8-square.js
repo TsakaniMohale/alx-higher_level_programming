fba5dadfd03b98eec76f8e3100bde72acb7d370e
Tsakani Mohale <tsakanimohale4@gmail.com>
2024-03-02 18:07:55 +0000

    0x12. JavaScript - Warm up

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const args = parseInt(process.argv[2]);
+let square = '';
+let width = 0;
+if (isNaN(args)) console.log('Missing size');
+else {
+  for (let height = 0; height < args; height++) {
+    while (width < args) {
+      square += 'X';
+      width++;
+    }
+    console.log(square);
+  }
+}
